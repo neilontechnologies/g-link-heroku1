402cbde5cfc216d96884093604acb6d7c0528cc2
Abhishek Patinge <abhishekpatinge2000@gamil.com>
2024-12-05 15:01:05 +0530

    sdsd

--- app.js
+++ app.js
@@ -146,54 +146,17 @@ const migrateSalesforce = async (sfFileId, googleDriveAccessKey, googleDriveSecr
             // Get google drive folder id
             const googleDriveFolderId = createGoogleDriveFolderResult1.data.split('/')[2];// get last / id
 
-            // console.log('GOOGLE DRIVE FOLDER ID', googleDriveFolderId);
-            // // Upload file into google drive 
-          ///CODE FOR UPLOAD FILE TEST
-          //const createFileMigrationLogResult =  createFileMigrationLog(salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, salesforceAccessToken, sfNamespace);
-
-          const drive = google.drive({ version: 'v3', auth: googleDriveAccessToken });
- 
-            // Get meta tags
-            var fileMetaTags = {};
-            const metatype = 'google';
-
-            // Prepare google drive metadata map
-            Object.entries(googleDriveFileMetadata).forEach(([filedAPIName, value]) => {
-              var fieldAPI = filedAPIName;
-              var metaFieldAPI = 'x-' + metatype + '-meta-' + fieldAPI.toLowerCase();
-              if (googleDriveFileMetadata[fieldAPI] !== undefined && googleDriveFileMetadata[fieldAPI] !== null) {
-                fileMetaTags[metaFieldAPI] = googleDriveFileMetadata[fieldAPI].toString();
-            } else {
-                fileMetaTags[metaFieldAPI] = '';
-            }
-          });
-
-          //const createFileMigrationLogResult =  createFileMigrationLog(salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, JSON.stringify(googleDriveFolderId), sfNamespace);
-
-              // Prepare metadata to store in google drive file
-            // const googleDriveFolderIds = [];
-            // googleDriveFolderIds.push(googleDriveFolderId);
-            // const fileMetaData = {
-            //   name: googleDriveFileTitle,
-            //   parents: googleDriveFolderIds, 
-            //   mimeType: gFile[sfNamespace + 'Content_Type__c'],
-            //   properties: fileMetaTags
-            // };
-
-            // console.log('VALUE');
-
           //const createFileMigrationLogResult =  createFileMigrationLog('00DDn000003ouaT!AQoAQIJQYj5VolJSTr5KCtklDspWqqidQopVpxfhW5Nqdd9evSiuYgYOjNnU8cPdhSCJv0kamd7OXldzJKV9dLEPF0MB5YK2', 'https://glinkdev-2-dev-ed.develop.my.salesforce.com', '068Dn00000EcDHsIAN', '06ADn00000O2NI7MAN', JSON.stringify(googleDriveAccessToken), '');
           const response = await uploadFileToGoogleDrive(googleDriveAccessToken, getSalesforceFileResult, googleDriveFolderId, googleDriveFileTitle, gFile, sfNamespace, salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, sfCreateLog, googleDriveFileMetadata);
+          
+            if(response.status == 200){
+              if(response && response.data && response.data.id){
+                const googleDriveFileId = response.data.id;
 
-
-            // if(response.status == 200){
-            //   if(response && response.data && response.data.id){
-            //     const googleDriveFileId = response.data.id;
-
-            //     // Create g file record if file is successfully uploaded into google drive
-            //     //const createGFilesInSalesforceResult = await createGFilesInSalesforce(salesforceAccessToken, instanceUrl, googleDriveBucketName, googleDriveFilePath, sfFileSize, sfContentDocumentId, sfFileId, sfContentDocumentLinkId, sfNamespace, sfDeleteFile, sfCreateLog, gFile, googleDriveFileId);
-            //   }
-            // }
+                // Create g file record if file is successfully uploaded into google drive
+                //const createGFilesInSalesforceResult = await createGFilesInSalesforce(salesforceAccessToken, instanceUrl, googleDriveBucketName, googleDriveFilePath, sfFileSize, sfContentDocumentId, sfFileId, sfContentDocumentLinkId, sfNamespace, sfDeleteFile, sfCreateLog, gFile, googleDriveFileId);
+              }
+            }
           }
         }
       } else{
