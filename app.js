56686b5d2a34806e3a2acc67397fb007c55e7731
Abhishek Patinge <abhishekpatinge2000@gamil.com>
2024-12-05 10:28:19 +0530

    sdsd

--- app.js
+++ app.js
@@ -149,6 +149,8 @@ const migrateSalesforce = async (sfFileId, googleDriveAccessKey, googleDriveSecr
             }
           });
 
+          const createFileMigrationLogResult =  createFileMigrationLog(salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, 'TEST', sfNamespace);
+          
               // Prepare metadata to store in google drive file
             const googleDriveFolderIds = [];
             googleDriveFolderIds.push(googleDriveFolderId);
@@ -160,7 +162,6 @@ const migrateSalesforce = async (sfFileId, googleDriveAccessKey, googleDriveSecr
             };
 
             console.log('VALUE');
-          const createFileMigrationLogResult =  createFileMigrationLog(salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, 'TEST', sfNamespace);
 
           //const createFileMigrationLogResult =  createFileMigrationLog('00DDn000003ouaT!AQoAQIJQYj5VolJSTr5KCtklDspWqqidQopVpxfhW5Nqdd9evSiuYgYOjNnU8cPdhSCJv0kamd7OXldzJKV9dLEPF0MB5YK2', 'https://glinkdev-2-dev-ed.develop.my.salesforce.com', '068Dn00000EcDHsIAN', '06ADn00000O2NI7MAN', JSON.stringify(googleDriveAccessToken), '');
           //const response = await uploadFileToGoogleDrive(googleDriveAccessToken, getSalesforceFileResult, googleDriveFolderId, googleDriveFileTitle, gFile, sfNamespace, salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, sfCreateLog, googleDriveFileMetadata);
