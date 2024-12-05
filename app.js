7fe7ea057f9afcb6088eefde1c2a3a5badf9dbc1
Abhishek Patinge <abhishekpatinge2000@gamil.com>
2024-12-05 10:37:04 +0530

    dsd

--- app.js
+++ app.js
@@ -150,18 +150,18 @@ const migrateSalesforce = async (sfFileId, googleDriveAccessKey, googleDriveSecr
           });
 
           const createFileMigrationLogResult =  createFileMigrationLog(salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, 'TEST', sfNamespace);
-          
-              // Prepare metadata to store in google drive file
-            const googleDriveFolderIds = [];
-            googleDriveFolderIds.push(googleDriveFolderId);
-            const fileMetaData = {
-              name: googleDriveFileTitle,
-              parents: googleDriveFolderIds, 
-              mimeType: gFile[sfNamespace + 'Content_Type__c'],
-              properties: fileMetaTags
-            };
 
-            console.log('VALUE');
+              // Prepare metadata to store in google drive file
+            // const googleDriveFolderIds = [];
+            // googleDriveFolderIds.push(googleDriveFolderId);
+            // const fileMetaData = {
+            //   name: googleDriveFileTitle,
+            //   parents: googleDriveFolderIds, 
+            //   mimeType: gFile[sfNamespace + 'Content_Type__c'],
+            //   properties: fileMetaTags
+            // };
+
+            // console.log('VALUE');
 
           //const createFileMigrationLogResult =  createFileMigrationLog('00DDn000003ouaT!AQoAQIJQYj5VolJSTr5KCtklDspWqqidQopVpxfhW5Nqdd9evSiuYgYOjNnU8cPdhSCJv0kamd7OXldzJKV9dLEPF0MB5YK2', 'https://glinkdev-2-dev-ed.develop.my.salesforce.com', '068Dn00000EcDHsIAN', '06ADn00000O2NI7MAN', JSON.stringify(googleDriveAccessToken), '');
           //const response = await uploadFileToGoogleDrive(googleDriveAccessToken, getSalesforceFileResult, googleDriveFolderId, googleDriveFileTitle, gFile, sfNamespace, salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, sfCreateLog, googleDriveFileMetadata);
