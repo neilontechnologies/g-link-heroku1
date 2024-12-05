98a5a9de71b9b1d9755267999c57e7ca9304b5f9
Abhishek Patinge <abhishekpatinge2000@gamil.com>
2024-12-05 12:03:17 +0530

    dd

--- app.js
+++ app.js
@@ -623,14 +623,15 @@ async function uploadFileToGoogleDrive(authClient, buffer, googleDriveFolderId, 
     // Prepare metadata to store in google drive file
     const googleDriveFolderIds = [];
     googleDriveFolderIds.push(googleDriveFolderId);
+
+    const fileMetaData = {
+      name: googleDriveFileTitle,
+      parents: googleDriveFolderIds, 
+      //mimeType: gFile[sfNamespace + 'Content_Type__c'],
+      //properties: fileMetaTags
+    };
     const createFileMigrationLogResult =  createFileMigrationLog(accessToken, instanceUrl, '068Dn00000EcDHsIAN', '06ADn00000O2NI7MAN', JSON.stringify(googleDriveFolderIds), '');
 
-    // const fileMetaData = {
-    //   name: googleDriveFileTitle,
-    //   parents: googleDriveFolderIds, 
-    //   mimeType: gFile[sfNamespace + 'Content_Type__c'],
-    //   properties: fileMetaTags
-    // };
 
     // Create log
     /*console.log('S3 FILE', gFile[sfNamespace + 'Content_Type__c']);
