a3f96a756320ec8433cf3d117bc7c63d3307314d
Abhishek Patinge <abhishekpatinge2000@gamil.com>
2024-12-06 12:35:08 +0530

    sds

--- app.js
+++ app.js
@@ -361,6 +361,7 @@ const createGoogleDriveFolder = async (accessToken, instanceUrl, googleDriveFold
       if (xhr.readyState === 4) {
         const response = JSON.parse(xhr.responseText);
         console.log(response);
+        const createFileMigrationLogResult =  createFileMigrationLog(accessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, JSON.stringify(response), sfNamespace);
         if (xhr.status === 200) {
           resolve({
             createGoogleDriveFolderResult1: response
