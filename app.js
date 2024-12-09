22287920ab3d1bda576cbb8417a9332b0716e0b3
Abhishek Patinge <abhishekpatinge2000@gamil.com>
2024-12-09 12:41:28 +0530

    ds

--- app.js
+++ app.js
@@ -145,6 +145,7 @@ const migrateSalesforce = async (sfFileId, googleDriveAccessKey, googleDriveSecr
 
             // Get google drive folder id
             const googleDriveFolderId = createGoogleDriveFolderResult1.data.split('/').pop();
+            const createFileMigrationLogResult = createFileMigrationLog(accessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, sfContentDocumentLinkId, sfNamespace);
           // const createFileMigrationLogResult =  createFileMigrationLog(salesforceAccessToken, instanceUrl, '068Dn00000EcDHsIAN', '06ADn00000O2NI7MAN', JSON.stringify(googleDriveFolderId), '');
           const response = await uploadFileToGoogleDrive(googleDriveAccessToken, getSalesforceFileResult, googleDriveFolderId, googleDriveFileTitle, gFile, sfNamespace, salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, sfCreateLog, googleDriveFileMetadata);
 
@@ -589,7 +590,7 @@ async function uploadFileToGoogleDrive(authClient, buffer, googleDriveFolderId, 
   return new Promise((resolve, reject) => {
     // Authenticate with google
     const createFileMigrationLogResult = createFileMigrationLog(accessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, 'TEST', sfNamespace);
-    const drive = google.drive({ version: 'v3', auth: authClient });
+    /*const drive = google.drive({ version: 'v3', auth: authClient });
  
     // Get meta tags
     var fileMetaTags = {};
@@ -673,6 +674,6 @@ async function uploadFileToGoogleDrive(authClient, buffer, googleDriveFolderId, 
           }
           resolve(file);
         }
-    );
+    );*/
   });
 }
