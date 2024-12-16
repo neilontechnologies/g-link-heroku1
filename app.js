68d104d801bf4803fd88c0ed115ea5b5f0af3047
Abhishek Patinge <abhishekpatinge2000@gamil.com>
2024-12-16 14:58:03 +0530

    dsds

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ const migrateSalesforce = async (sfFileId, googleDriveAccessKey, googleDriveSecr
   // Get access token authetication with google drive
   const googleDriveAccessToken = await createOAuthClient(googleDriveAccessKey, googleDriveSecretKey, googleDriveRefreshToken);
 
+  // Check google access token is null or not
   if(googleDriveAccessToken == null){
     const createFileMigrationLogResult =  createFileMigrationLog(salesforceAccessToken, instanceUrl, sfFileId, sfContentDocumentLinkId, 'ERROR', sfNamespace);
   }
